ff70217e35d957390f31e4b8cb4357d0049f3878
Mário Henrique do Nascimento <user.email>
2016-07-15 12:59:17 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/lifeundercontrol/LifeundercontrolUiApplication.java
@@ -0,0 +1,12 @@
+package br.com.lifeundercontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeundercontrolUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeundercontrolUiApplication.class, args);
+	}
+}
